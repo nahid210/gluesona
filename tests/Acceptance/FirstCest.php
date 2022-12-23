a92e269eae3756a9c8ffd22468c92b4481660332
nahid21o <nahid.nets@gmail.com>
2022-12-23 11:37:32 +0600

    ok2

---
+++ tests/Acceptance/FirstCest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class FirstCest
+{
+    public function frontpageWorks(AcceptanceTester $I)
+    {
+        
+    }
+}
